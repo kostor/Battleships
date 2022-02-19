e1049f062d34dfa023b011c4da9b02b15b793680
kostor <yardensnw@gmail.com>
2022-02-19 21:20:08 +0200

    Added Boat class in seperate file. 2 attributes: type, size. 3 methods: init, get_size, get_type

---
+++ Boat.py
@@ -0,0 +1,12 @@
+class Boat:
+    def __init__(self, type, size):
+        self.type = type
+        self.size = size
+    def get_size(self):
+        return self.size
+    def get_type(self):
+        return self.type
+
+
+
+
