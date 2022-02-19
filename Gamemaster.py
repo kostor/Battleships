fcc38d28539a7ecf3d1e7d36b25305a8ff7fb501
kostor <yardensnw@gmail.com>
2022-02-19 23:46:19 +0200

    initial commit. Creates the object. Basic functunality so far: players orientations are randomized. TODO: create randomized valid locations based on the orientations, draw map (new object?), turns logic, winning cond., etc'

--- Gamemaster.py
+++ Gamemaster.py
@@ -1,5 +1,17 @@
-class my_class(object):
-    pass
+import random
+class Gamemaster:
+    def __init__(self):
+        self.player1_locations = []
+        self.player2_locations = []
+        self.player1_orientations = []
+        self.player2_orientations = []
+        for index in range(5):
+            self.player1_orientations.append(random.randint(0,1))
+        for index in range(5):
+            self.player2_orientations.append(random.randint(0,1))
+
+        self.plaeyer1 = Player()
+
 
 
 
