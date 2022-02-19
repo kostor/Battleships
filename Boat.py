aebcda7e35ef9e2c881d384e8d01074b86a3dae0
kostor <yardensnw@gmail.com>
2022-02-19 22:26:37 +0200

    Added location for each boat (where it starts, default is 0,0) and orientation (0 - horizontal, default. 1 - vertical)

--- Boat.py
+++ Boat.py
@@ -1,7 +1,9 @@
 class Boat:
-    def __init__(self, type, size):
+    def __init__(self, type, size, location = {"X": 0, "Y": 0}, orientation = 0):
         self.type = type
         self.size = size
+        self.location = location
+        self.orientation = orientation
     def get_size(self):
         return self.size
     def get_type(self):
