00474a3987abff16399aa01852f06de0bc0f070c
kostor <yardensnw@gmail.com>
2022-02-21 23:20:41 +0200

    fixed half-done gamemaster functionality that produced only 1 coordinate for ships. More changes from the main gamemaster branch is in.

--- Gamemaster.py
+++ Gamemaster.py
@@ -1,9 +1,9 @@
 import random
-import Player
+from Player import Player
 class Gamemaster:
 
     
-    boat_sizes = [5, 4, 3, 3, 2] # boat sizes assuming usual 
+    boat_sizes = [5, 4, 3, 3, 2] # boat sizes assuming usual game set
     fleet_size = len(boat_sizes) # this will make sure that we just 
                                  # need to change the boat configuration and the fleet size will automatically be updated
     def __init__(self):
@@ -15,22 +15,26 @@ class Gamemaster:
         # randomize orientations for each ship in the fleet
         for index in range(Gamemaster.fleet_size):
             self.player1_orientations.append(random.randint(0,1))
-        for index in range(Gamemaster.fleet_size):
             self.player2_orientations.append(random.randint(0,1))
-
+        #for index in range(Gamemaster.fleet_size):
+            
+        
+        # randomize locations based on orientations (to prevent index overflow when creating the map)
         for index in range(Gamemaster.fleet_size):
-            if self.player1_orientations[index] == 0: # if ship is horizontal
-                self.player1_locations.append([random.randint(1,10), random.randint(1,10-Gamemaster.boat_sizes[index]+1)])
-            elif self.player1_orientations[index] == 1: # if ship is vertical
-                self.[player1_locations.append([random.randint(1,10-Gamemaster.boat_sizes[index]+1)])
-            else:
-                
+            if self.player1_orientations[index] != 1: # if ship is horizontal. This includes extreme cases where orientation is not 0 or 1
+                self.player1_locations.append({"X": random.randint(1,10 - Gamemaster.boat_sizes[index]+1), "Y": random.randint(1,10)})
+            else: # if ship is vertical
+                self.player1_locations.append({"X": random.randint(1,10), "Y":random.randint(1,10-Gamemaster.boat_sizes[index]+1)})
 
+            if self.player2_orientations[index] != 1:  # if ship is horizontal. This includes extreme cases where orientation is not 0 or 1
+                self.player2_locations.append({"X": random.randint(1,10 - Gamemaster.boat_sizes[index]+1), "Y": random.randint(1,10)})
+            else: # if ship is vertical
+                self.player2_locations.append({"X": random.randint(1,10), "Y":random.randint(1,10-Gamemaster.boat_sizes[index]+1)})
+        
+        self.player1 = Player(self.player1_locations, self.player1_orientations)
+        self.player2 = Player(self.player2_locations, self.player2_orientations)
 
-
-        self.player1 = Player()
-
-
-
-
-
+    def get_player1(self):
+        return self.player1
+    def get_player2(self):
+        return self.player2
