64cbb18033021606146c15b6095ba067828cd0b6
kostor <yardensnw@gmail.com>
2022-03-01 20:09:41 +0200

    No overlapping ships - beta version
    This beta version of no overlapping ships still needs more debugging.

--- Battleships.py
+++ Battleships.py
@@ -1,12 +1,5 @@
 from Gamemaster import Gamemaster 
-from Map import Map
+
 
 gamemaster = Gamemaster()
 
-boats_map = Map(gamemaster.player1_locations,gamemaster.player1_orientations)
-print(boats_map)
-print('----------------')
-print(boats_map.get_marker([1,2,3,4,5],[1,3]))
-print('----------------')
-print(boats_map.reset_map())
-print("Ready? Set, Play!")
